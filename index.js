48e58f3d1f870be53333009b7638bfc60e174735
sairessy <sairessy@gmail.com>
2021-11-01 15:07:55 -1000

    msg

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 const PORT = 3000 || process.env.PORT;
+
+const collections = {
+  visits: new Datastore('./database/collections/visits.db')
+};
+
+collections.visits.loadDatabase();
 
 app.listen(PORT, () => { });
 
 app.get('/aprova', async (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const data = { date: Date.now(), ip };
+  collections.visits.insert(data, (err, doc) => { });
   res.redirect('https://drive.google.com/file/d/1MJBc5ooLyfcvuqbsYDSzv-__3c-eu-e4/view?usp=sharing');
 });
